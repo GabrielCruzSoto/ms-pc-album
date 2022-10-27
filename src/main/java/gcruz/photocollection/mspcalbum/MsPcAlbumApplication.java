4498d542201b52dd174534bfd5fa3aaff2691b27
Gabriel Cruz S <gabrielcruzsoto@gmail.com>
2022-10-27 01:33:47 -0300

    Project Initial

---
+++ src/main/java/gcruz/photocollection/mspcalbum/MsPcAlbumApplication.java
@@ -0,0 +1,13 @@
+package gcruz.photocollection.mspcalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPcAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPcAlbumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gcruz/photocollection/mspcalbum/MsPcAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package gcruz.photocollection.mspcalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPcAlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
